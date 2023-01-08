43f6ea7cb23601be0f92af37e75def9a72709d70
Progna_Labony_Roy <labony.progna@gmail.com>
2023-01-08 11:21:42 +0600

    header and navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,37 @@
 import React from "react";
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const Header = () => {
   return (
-    <div>
-      <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""></img>
-      <div>
+    <div className="header">
+      <img
+        className="header_logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      ></img>
+      <div className="header_search">
         <input className="header_searchInput" type="text"></input>
-        {/* logo */}
+        <SearchIcon className="header_searchIcon"></SearchIcon>
       </div>
-      
+
       <div className="header_nav">
-        <div>
-            <span>Hello</span>
+        <div className="header_option">
+          <span className="lineOne">Hello</span>
+          <span className="lineTwo">Sign in</span>
+        </div>
+        <div className="header_option">
+          <span className="lineOne">Returns</span>
+          <span className="lineTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="lineOne">Your</span>
+          <span className="lineTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+            <ShoppingCartIcon></ShoppingCartIcon>
+            <span className="lineTwo header_basketCount">0</span>
         </div>
       </div>
     </div>
